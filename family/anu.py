44b22cbb92f71cd9110c65fe7dde00ae9d6b7985
Whois <113191030+dratx1@users.noreply.github.com>
2025-09-26 08:37:29 +0700

    anu.py

---
+++ family/anu.py
@@ -0,0 +1,18 @@
+[
+    {
+        "family_name": "Bundling",
+        "family_code": "6bcc96f4-f196-4e8f-969f-e45a121d21bd",
+        "is_enterprise": false,
+        "variant_name": "Bundling",
+        "option_name": "Bundling Bundling 60GB Setahun YouTube",
+        "order": 1
+    },
+    {
+        "family_name": "Bundling",
+        "family_code": "6bcc96f4-f196-4e8f-969f-e45a121d21bd",
+        "is_enterprise": false,
+        "variant_name": "Bundling",
+        "option_name": "Bundling Bundling 45GB Setahun",
+        "order": 2
+    }
+]
